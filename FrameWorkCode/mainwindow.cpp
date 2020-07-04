8862811f7814e94c8e8c52f67ca2750577af0f94
svtsanoj <svtsanoj@gmail.com>
2020-07-04 17:25:53 +0530

    Levenshtein bug fixed

--- FrameWorkCode/mainwindow.cpp
+++ FrameWorkCode/mainwindow.cpp
@@ -2735,7 +2735,7 @@ void MainWindow::on_pushButton_clicked()
         s2 = t.toUtf8().constData();
         sFile3.close();
     }
-    int i,j,l1,l2,t,track,levinshtein; float accuracy;
+    int i,j,l1,l2,t,track,levenshtein; float accuracy;
 
        //take the strings as input
 //       char s1[] = string1;
@@ -2743,34 +2743,36 @@ void MainWindow::on_pushButton_clicked()
        //stores the lenght of strings s1 and s2
        l1 = s1.length();
        l2= s2.length();
-      char char1[l1+1];
-      char char2[l2+1];
-      int dist[l2][l1];
 
-      strcpy(char1, s1.c_str());
-      strcpy(char2, s2.c_str());
+//      char char1[l1+1];
+//      char char2[l2+1];
+//      int dist[l2][l1];
+
+//      strcpy(char1, s1.c_str());
+//      strcpy(char2, s2.c_str());
 
        qDebug() <<l1 << "l1";
        qDebug() <<l2 << "l2";
-       for(i=0;i<=l1;i++) {
-          dist[0][i] = i;
-       }
-       for(j=0;j<=l2;j++) {
-          dist[j][0] = j;
-       }
-       for (j=1;j<=l1;j++) {
-          for(i=1;i<=l2;i++) {
-             if(char1[i-1] == char2[j-1]) {
-                track= 0;
-             } else {
-                track = 1;
-             }
-             t = MIN((dist[i-1][j]+1),(dist[i][j-1]+1));
-             dist[i][j] = MIN(t,(dist[i-1][j-1]+track));
-          }
-       }
-       levinshtein = dist[l2][l1];
-       qDebug() <<levinshtein << "levinshtein";
-       accuracy = ((l1-levinshtein)/l1)*100;
+//       for(i=0;i<=l1;i++) {
+//          dist[0][i] = i;
+//       }
+//       for(j=0;j<=l2;j++) {
+//          dist[j][0] = j;
+//       }
+//       for (j=1;j<=l1;j++) {
+//          for(i=1;i<=l2;i++) {
+//             if(char1[i-1] == char2[j-1]) {
+//                track= 0;
+//             } else {
+//                track = 1;
+//             }
+//             t = MIN((dist[i-1][j]+1),(dist[i][j-1]+1));
+//             dist[i][j] = MIN(t,(dist[i-1][j-1]+track));
+//          }
+//       }
+       levenshtein = editDist(s1,s2);
+       qDebug() <<levenshtein << "levenshtein";
+       accuracy = ((float)(l1-levenshtein)/(float)l1)*100;
+       qDebug() <<accuracy << "accuracy";
        ui->lineEdit_2->setText(QString::number(accuracy) + "%");
 }
