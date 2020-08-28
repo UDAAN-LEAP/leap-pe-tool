042562f77af11072efde8645772e573dd922566e
Sanoj Senthilvelavan <38166266+svtsanoj@users.noreply.github.com>
2020-08-28 16:48:55 +0530

    Update mainwindow.cpp
    curr_browser check for all font properties

--- FrameWorkCode/mainwindow.cpp
+++ FrameWorkCode/mainwindow.cpp
@@ -2124,6 +2124,8 @@ void MainWindow::on_actionEnglish_triggered()
 
 void MainWindow::on_actionBold_triggered() //modified
 {
+	if(!curr_browser)
+		return;
     QTextCharFormat format;
     format.setFontWeight(QFont::Bold);
     curr_browser->textCursor().mergeCharFormat(format);
@@ -2131,6 +2133,8 @@ void MainWindow::on_actionBold_triggered() //modified
 
 void MainWindow::on_actionUnBold_triggered() //modified
 {
+	if(!curr_browser)
+		return;
     QTextCharFormat format;
     format.setFontWeight(QFont::Normal);
     curr_browser->textCursor().mergeCharFormat(format);
@@ -2138,20 +2142,28 @@ void MainWindow::on_actionUnBold_triggered() //modified
 
 void MainWindow::on_actionLeftAlign_triggered() //modified
 {
+	if(!curr_browser)
+		return;
     curr_browser->setAlignment(Qt::AlignLeft);
 }
 
 void MainWindow::on_actionRightAlign_triggered() //modified
 {
+	if(!curr_browser)
+		return;
     curr_browser->setAlignment(Qt::AlignRight);
 }
 
 void MainWindow::on_actionCentreAlign_triggered()
 {
+	if(!curr_browser)
+		return;
     curr_browser->setAlignment(Qt::AlignCenter);
 }
 void MainWindow::on_actionJusitfiedAlign_triggered()
 {
+	if(!curr_browser)
+		return;
     auto cursor = curr_browser->textCursor();
     auto selected = cursor.selection();
     cursor.removeSelectedText();
@@ -2165,6 +2177,8 @@ void MainWindow::on_actionJusitfiedAlign_triggered()
 
 void MainWindow::on_actionAllFontProperties_triggered()
 {
+	if(!curr_browser)
+		return;
     QFont initialFont = curr_browser->font();
     auto pointsize = curr_browser->fontPointSize();
     if(pointsize) initialFont.setPointSize(pointsize);
@@ -2180,6 +2194,8 @@ void MainWindow::on_actionAllFontProperties_triggered()
 
 void MainWindow::on_actionFontBlack_triggered()
 {
+	if(!curr_browser)
+		return;
     curr_browser->setTextColor(Qt::black);
 }
 
@@ -2203,11 +2219,15 @@ void MainWindow::on_actionSubscript_triggered() {
 }
 void MainWindow::on_actionInsert_Horizontal_Line_triggered()
 {
+	if(!curr_browser)
+		return;
     curr_browser->insertHtml("<hr>");
 }
 
 void MainWindow::on_actionLineSpace_triggered()
 {
+	if(!curr_browser)
+		return;
     QTextCursor cursor = curr_browser->textCursor();
     QTextBlockFormat format = cursor.blockFormat();
     double lineHeight = format.lineHeight()/100;
@@ -2226,6 +2246,8 @@ void MainWindow::on_actionLineSpace_triggered()
 
 void MainWindow::on_actionInsert_Tab_Space_triggered()
 {
+	if(!curr_browser)
+		return;
     curr_browser->insertPlainText("    ");
 }
 
