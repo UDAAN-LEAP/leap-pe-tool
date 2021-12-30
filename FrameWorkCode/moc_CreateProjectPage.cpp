afd73167a2dcee57f9fc2c5566a7ec0094a13de7
“akansharawat27” <akansharawat.ind@gmail.com>
2021-12-30 20:01:47 +0530

    Fixed Toolbar Icon Names Color

--- FrameWorkCode/moc_CreateProjectPage.cpp
+++ FrameWorkCode/moc_CreateProjectPage.cpp
@@ -1,19 +1,18 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'CreateProjectPage.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <memory>
 #include "CreateProjectPage.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'CreateProjectPage.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.2. It"
+#error "This file was generated using the moc from 5.12.8. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -64,7 +63,7 @@ void CreateProjectPage::qt_static_metacall(QObject *_o, QMetaObject::Call _c, in
 }
 
 QT_INIT_METAOBJECT const QMetaObject CreateProjectPage::staticMetaObject = { {
-    QMetaObject::SuperData::link<QWizardPage::staticMetaObject>(),
+    &QWizardPage::staticMetaObject,
     qt_meta_stringdata_CreateProjectPage.data,
     qt_meta_data_CreateProjectPage,
     qt_static_metacall,
--- FrameWorkCode/moc_sender.cpp
+++ FrameWorkCode/moc_sender.cpp
@@ -1,12 +1,11 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'sender.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <memory>
 #include "SimpleMail/sender.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
@@ -14,7 +13,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'sender.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.2. It"
+#error "This file was generated using the moc from 5.12.8. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -198,7 +197,7 @@ void SimpleMail::Sender::qt_static_metacall(QObject *_o, QMetaObject::Call _c, i
 }
 
 QT_INIT_METAOBJECT const QMetaObject SimpleMail::Sender::staticMetaObject = { {
-    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    &QObject::staticMetaObject,
     qt_meta_stringdata_SimpleMail__Sender.data,
     qt_meta_data_SimpleMail__Sender,
     qt_static_metacall,
@@ -240,14 +239,14 @@ int SimpleMail::Sender::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 // SIGNAL 0
 void SimpleMail::Sender::smtpError(SmtpError _t1)
 {
-    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 
 // SIGNAL 1
 void SimpleMail::Sender::sslErrors(const QList<QSslError> & _t1)
 {
-    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
