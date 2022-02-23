ecae14a56979e7ec6afb10886e1ad66f20cfd564
sujaytorvi <sujay.torvi@gmail.com>
2022-02-23 22:13:42 +0530

    LoginPageView Documented

--- FrameWorkCode/loginpageview.cpp
+++ FrameWorkCode/loginpageview.cpp
@@ -1,8 +1,18 @@
+/*!
+ *\class LoginPageView
+ *\brief This class is used to authenticate the Github User
+ */
 #include "loginpageview.h"
 #include "ui_loginpageview.h"
 #include "mainwindow.h"
 #include <QMessageBox>
 
+/*!
+ * \fn LoginPageView::LoginPageView
+ * \brief Sets up the UI and sets the text edit for the password field so that the typed characters are
+ * not visible
+ * \param parent
+ */
 LoginPageView::LoginPageView(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LoginPageView)
@@ -10,20 +20,29 @@ LoginPageView::LoginPageView(QWidget *parent) :
     ui->setupUi(this);
     ui->passwordEdit->setEchoMode(QLineEdit::Password);
 }
-
+/*!
+ * \brief LoginPageView::~LoginPageView
+ */
 LoginPageView::~LoginPageView()
 {
     delete ui;
 }
 
-
+/*!
+ * \fn LoginPageView::on_loginButton_clicked
+ * \brief This function Authenticates the user
+ */
 void LoginPageView::on_loginButton_clicked()
 {
+
+
+    //! Get credentials from user
 
     QString usernameString= ui->usernameEdit->text();
     QString passwordString= ui->passwordEdit->text();
     //ui->usernameEdit->clear();
     ui->passwordEdit->clear();
+
 
     //QString projectId = ""; //should be added in project.xml file and fetched from there
     QString userId, userRole;  //fetch on sign in   //add location
@@ -37,6 +56,10 @@ void LoginPageView::on_loginButton_clicked()
 
     QJsonDocument userDocument = QJsonDocument::fromJson(userData);
     QJsonObject userObj = userDocument.object();
+
+    //! Get userid and password of Github and authenticate user. If userid is undefined print message to
+    //! user that login was unsucessful
+
     if(!userObj.value(usernameString).isUndefined()) {
         QJsonObject user = userObj.value(usernameString).toObject();
 //      QString username = user["github userid"].toString();
