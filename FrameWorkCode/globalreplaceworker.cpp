eeb80dce1e46b14f73d8a55072c2b41aca153d8e
sujaytorvi <sujay.torvi@gmail.com>
2022-04-20 16:39:10 +0530

    Sanitized Regex

--- FrameWorkCode/globalreplaceworker.cpp
+++ FrameWorkCode/globalreplaceworker.cpp
@@ -61,11 +61,12 @@ int GlobalReplaceWorker::writeGlobalCPairsToFiles(QString file_path, QMap<QStrin
             //qDebug() << "grmIterator Key : " <<grmIterator.key()<<"grmIterator.value : "<<grmIterator.value();
             if((grmIterator.key().split(" ", QString::SkipEmptyParts).count() == 1) || (grmIterator.value().split(" ", QString::SkipEmptyParts).count() == 1))
             {
-                qDebug() << "Word replacement";
-                QString pattern = "(\\b)"+grmIterator.key()+"(\\b)"; // \b is word boundary, for cpp compilers an extra \ is required before \b, refer to QT docs for details
+                QString sanstr = QRegExp::escape(grmIterator.key());    //sanitized string
+                QString pattern = "(\\b)"+sanstr+"(\\b)"; // \b is word boundary, for cpp compilers an extra \ is required before \b, refer to QT docs for details
                 QRegExp re(pattern);
                 QString replacementString = re.cap(1) + grmIterator.value() + re.cap(2); // \1 would be replace by the first paranthesis i.e. the \b  and \2 would be replaced by the second \b by QT Regex
                 std::string str = replacementString.toStdString();
+                qDebug() << "San : " << sanstr << "\nRegexp : " << re;
                 QString::fromStdString(str).toUtf8();
                 //QString replacementString1 = QString::fromStdString(str).trimmed();
                 QString replacementString1 = "<span style = \"background-color:#ffff00;\">" + QString::fromStdString(str).trimmed() + "</span>";
@@ -88,6 +89,7 @@ int GlobalReplaceWorker::writeGlobalCPairsToFiles(QString file_path, QMap<QStrin
 
         }
     in << s1;
+    f.flush();
     f.close();
 
     return tot_replaced;
