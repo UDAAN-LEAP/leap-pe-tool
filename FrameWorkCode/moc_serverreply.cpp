e26614711caa08b60814a28666d7bc856a32dd46
“DHRUV” <dhruvmpatel096@gmail.com>
2022-07-01 22:13:49 +0530

    Added 2 Special Symbols

--- FrameWorkCode/moc_serverreply.cpp
+++ FrameWorkCode/moc_serverreply.cpp
@@ -1,18 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'serverreply.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "SimpleMail/serverreply.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'serverreply.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.12.8. It"
+#error "This file was generated using the moc from 5.15.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -85,7 +86,7 @@ void SimpleMail::ServerReply::qt_static_metacall(QObject *_o, QMetaObject::Call 
 }
 
 QT_INIT_METAOBJECT const QMetaObject SimpleMail::ServerReply::staticMetaObject = { {
-    &QObject::staticMetaObject,
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
     qt_meta_stringdata_SimpleMail__ServerReply.data,
     qt_meta_data_SimpleMail__ServerReply,
     qt_static_metacall,
