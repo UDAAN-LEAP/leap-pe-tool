e583bba81b2647f58b943c2adbdb8a5458e90876
Sadam Hussain <66163807+Sadam452@users.noreply.github.com>
2022-11-08 15:52:43 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OpenOCRCorrect'
+copyright = 'OpenOCRCorrect IITB'
+author = 'IITB Admin'
 
 release = '0.1'
-version = '0.1.0'
+version = '3.0.0'
 
 # -- General configuration
 
